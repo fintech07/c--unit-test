81eda82c7a0312496ff64e6a69d9234278e2eea7
fintech <funnydeveloper100@gmail.com>
2020-10-22 09:20:01 +0800

    parameterized test

--- NetCoreTesting/UnitTest1.cs
+++ NetCoreTesting/UnitTest1.cs
@@ -37,5 +37,14 @@ namespace NetCoreTesting.Tests
             var result = _math.Max(2, 2);
             Assert.That(result, Is.EqualTo(2));
         }
+
+        [Test]
+        [TestCase(2, 1, 2)]
+        [TestCase(1, 2, 2)]
+        [TestCase(2, 2, 2)]
+        public void Max_WhenCalled_ReturnGreaterArgument(int a, int b, int expectation){
+            var result = _math.Max(a, b);
+            Assert.That(result, Is.EqualTo(expectation));
+        }
     }
 }
